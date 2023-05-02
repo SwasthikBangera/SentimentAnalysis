80daef24683ef22ae95444dc657a430eda7f9189
Swasthik Bangera <swasthik.bangera@gmail.com>
2023-05-02 09:31:09 -0700

    Python File Upload

---
+++ SentimentAnalysis.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 14 09:40:39 2023
+
+@author: SwasthikBangera
+
+"""
+# Importing dependencies
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+
+
+# Importing the dataset
+
+senti_df = pd.read_csv('') 
+
